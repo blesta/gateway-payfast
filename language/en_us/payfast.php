bdd4f5c6d34e8d8f08de936aa62900474a9f69e8
Abdy Franco <yosoy@abdyfran.co>
2022-10-14 17:03:29 -0600

    Initial Commit

---
+++ language/en_us/payfast.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * en_us language for the PayFast gateway.
+ */
+// Basics
+$lang['Payfast.name'] = 'PayFast';
+$lang['Payfast.description'] = 'Experience fast and secure online payments in South Africa from almost anywhere in the world.';
+
+
+// Errors
+$lang['Payfast.!error.merchant_id.valid'] = 'Merchant ID invalid.';
+$lang['Payfast.!error.merchant_key.valid'] = 'Merchant Key invalid.';
+
+
+// Settings
+$lang['Payfast.meta.merchant_id'] = 'Merchant ID';
+$lang['Payfast.meta.merchant_key'] = 'Merchant Key';
+$lang['Payfast.meta.sandbox'] = 'Sandbox';
+
+
+$lang['Payfast.buildprocess.submit'] = 'Pay with PayFast';
